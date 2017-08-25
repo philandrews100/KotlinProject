67605eca3f514164fc3cdf08ac4077e2bb73ed52
Philip Andrews <phil.andrews.100@gmail.com>
2017-08-25 20:18:17 +0100

    Fragments Commit 2
    - added in the main controller
    - setup the fragment controller
    - setup the core fragment functions
    - setup the main fragment functions and linked the navigation control to them

---
+++ app/src/main/java/smpl/code/me/kotlinproject/activities/main/interface/MainInterface.kt
@@ -0,0 +1,10 @@
+package smpl.code.me.kotlinproject.activities.main.`interface`
+
+/**
+ * Created by phil-apposing on 25/08/2017.
+ */
+interface MainInterface {
+    fun startDashboardFragment()
+    fun startHomeFragment()
+    fun startNotificationsFragment()
+}
--- app/src/main/java/smpl/code/me/kotlinproject/fragments/core/CoreFragment.kt
+++ app/src/main/java/smpl/code/me/kotlinproject/fragments/core/CoreFragment.kt
@@ -1,15 +1,19 @@
 package smpl.code.me.kotlinproject.fragments.core
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import smpl.code.me.kotlinproject.activities.main.control.MainControl
 
 /**
  * Created by phil-apposing on 25/08/2017.
  */
 abstract class CoreFragment: Fragment() {
+
+    private var mainControl: MainControl? = null
 
     abstract fun viewLayout(): Int
     abstract fun initView(view: View)
@@ -21,5 +25,15 @@ abstract class CoreFragment: Fragment() {
         setupView()
         return view
     }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+
+        mainControl = MainControl(context!!)
+    }
+
+    fun CoreFragment.startHomeFragment() = mainControl!!.startHomeFragment()
+    fun CoreFragment.startDashboardFragment() = mainControl!!.startDashboardFragment()
+    fun CoreFragment.startNotificationsFragment() = mainControl!!.startNotificationsFragment()
 
 }
